7b404583df7a8ce7e06e3b5463e8ef98c9183f91
Dharini <dharini.kannan1206@gmail.com>
2023-08-08 17:02:07 +0530

    First Commit

---
+++ src/test/java/Pack1/first_java.java
@@ -0,0 +1,8 @@
+package Pack1;
+
+public class first_java {
+public static void main(String a[])
+{
+	System.out.println("My Test program");
+}
+}
---
+++ src/test/java/Pack1/second_file.java
@@ -0,0 +1,8 @@
+package Pack1;
+
+public class second_file {
+public static void main(String a[])
+{
+	System.out.println("From secondfile");
+}
+}
